fbc8539e4558582594ac416ab47a22704b989600
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-06-09 20:50:52 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/sendpromotionemailjob/SendPromotionEmailJobApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.sendpromotionemailjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendPromotionEmailJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendPromotionEmailJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/sendpromotionemailjob/SendPromotionEmailJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.sendpromotionemailjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendPromotionEmailJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
